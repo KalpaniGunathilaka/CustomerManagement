e0b2285365d79d559812d81b24d3e67cf14ffe21
KalpaniGunathilaka <kalpanigunathilaka@gmail.com>
2026-06-07 14:41:49 +0530

    Added domain layer with entities and enums

---
+++ CustomerManagement.Domain/Enums/CustomerType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerManagement.Domain.Enums;
+
+public enum CustomerType : byte
+{
+    personal = 1,
+    business = 2
+}
